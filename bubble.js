1f9778b6045343ea7ffab063f68bb929581524cb
Spencer Taira <spencermtaira@gmail.com>
2022-12-16 12:12:59 -0800

    finished both algos

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+"use strict";
+
+/**
+ *  Given an array, bubblesort will sort the values in the array (smallest to largest)
+ *
+ *  Input: vals - array of vals
+ *  Output: vals - same array as input but sorted
+ */
+
+function bubbleSort(vals) {
+
+  for (let i = vals.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (vals[j] > vals[j+1]) {
+        [vals[j], vals[j+1]] = [vals[j+1], vals[j]];
+      }
+    }
+  }
+
+  return vals;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,48 @@
-function merge() {}
+"use strict";
 
-function mergeSort() {}
+/**
+ * Given two sorted arrays, function will return a new array with values from
+ * both arrays sorted
+ *
+ *  Input: (arr1, arr2) - (Array of values, Array of values)
+ *  Output: sorted array of values
+ */
+function merge(arr1, arr2) {
+  let leftIdx = 0;
+  let rightIdx = 0;
+  const sortedArr = [];
+
+  while (leftIdx < arr1.length && rightIdx < arr2.length) {
+    console.log('leftIdx', leftIdx, 'rightIdx', rightIdx);
+    if (arr1[leftIdx] > arr2[rightIdx]) {
+      sortedArr.push(arr2[rightIdx]);
+      rightIdx += 1;
+    } else {
+      sortedArr.push(arr1[leftIdx]);
+      leftIdx += 1;
+    }
+  }
+
+  return sortedArr.concat(arr1.slice(leftIdx), arr2.slice(rightIdx));
+}
+
+/**
+ * Given an array, function will return a new array with values sorted from
+ * smallest to largest
+ *
+ *  Input: vals - Array of values
+ *  Output: sortedVals - Array of sorted values (small -> large)
+ */
+function mergeSort(vals) {
+  if (vals.length <= 1) {
+    return vals;
+  }
+
+  const middle = Math.floor(vals.length / 2);
+  const left = mergeSort(vals.slice(0, middle));
+  const right = mergeSort(vals.slice(middle));
+
+  return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
